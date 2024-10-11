9028a16f168a71625d91277bd2efce1673b6ba30
logitech <15161035@sdmarsudirinibekasi.sch.id>
2024-10-12 01:18:22 +0700

    Upload pertama

---
+++ script.js
@@ -0,0 +1,10 @@
+let a = prompt("Haloooooooo, tuliss namaa lengkapppp yaaaa 💗");
+if (a != "Keyra Adinda Mylove Depari") {
+  alert("Namaa lengkappp yaa sayangg");
+  let b = prompt("Tuliss lagiiiii yaaaaa");
+  if (b == "Keyra Adinda Mylove Depari") {
+    alert("Pinterrrr bangettt ciii pacarrrr akuuu");
+  }
+} else {
+  alert("I LOVEEE YOUUU 💗");
+}
